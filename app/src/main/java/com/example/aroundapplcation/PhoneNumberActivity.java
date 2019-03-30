4de8a4501c5b2cd7c90c9636bfd508fb91c31497
Bulat Murtazin <random.finelife@gmail.com>
2019-03-30 16:15:14 +0300

    Development branch initial commit.

--- app/src/main/java/com/example/aroundapplcation/PhoneNumberActivity.java
+++ app/src/main/java/com/example/aroundapplcation/PhoneNumberActivity.java
@@ -1,14 +1,20 @@
 package com.example.aroundapplcation;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class PhoneNumberActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickConfirm(View view) {
+        Toast.makeText(this, "You pressed confirm", Toast.LENGTH_SHORT).show();
     }
 }
