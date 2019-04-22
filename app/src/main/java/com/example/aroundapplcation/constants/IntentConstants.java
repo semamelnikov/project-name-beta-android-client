64f2551668467dee7069dd7b156785db865f7c26
Semyon Melnikov <sema.melnikov@gmail.com>
2019-04-22 23:44:25 +0300

    Make constants final

--- app/src/main/java/com/example/aroundapplcation/constants/IntentConstants.java
+++ app/src/main/java/com/example/aroundapplcation/constants/IntentConstants.java
@@ -1,6 +1,7 @@
 package com.example.aroundapplcation.constants;
 
 public class IntentConstants {
-    public static String PHONE_NUMBER = "phoneNumber";
-    public static String REGISTRATION_SESSION_ID = "registrationSessionId";
+    public static final String PHONE_NUMBER = "phoneNumber";
+    public static final String REGISTRATION_SESSION_ID = "registrationSessionId";
+    public static final String BUSINESS_CARD_ID = "businessCardId";
 }
--- app/src/main/java/com/example/aroundapplcation/constants/SharedPreferencesConstants.java
+++ app/src/main/java/com/example/aroundapplcation/constants/SharedPreferencesConstants.java
@@ -1,8 +1,8 @@
 package com.example.aroundapplcation.constants;
 
 public class SharedPreferencesConstants {
-    public static String PHONE_NUMBER = "phoneNumber";
-    public static String ACCESS_TOKEN = "accessToken";
-    public static String REFRESH_TOKEN = "refreshToken";
-    public static String USER_ID = "userId";
+    public static final String PHONE_NUMBER = "phoneNumber";
+    public static final String ACCESS_TOKEN = "accessToken";
+    public static final String REFRESH_TOKEN = "refreshToken";
+    public static final String USER_ID = "userId";
 }
